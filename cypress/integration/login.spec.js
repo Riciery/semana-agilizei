48e5340d8b1000aae2e482615c6884bedeeb1a9c
RicierySpardelotti <you@example.com>
2021-11-17 22:52:44 -0300

    second commit

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe('Twitter Clone - Login', () => {
+
+    beforeEach(() => {
+        // cy.intercep
+        // 1. RouteMatcher - mapeamento, filtro da rota que a gente quer
+        // 2. RouteHandler (opcional) - controlar o que a rota vai retornar (mock)
+
+        // Request URL: https://res.cloudinary.com/douy56nkf/image/upload/v1588127894/twitter-build/bvxmlgckusmrwyivsnzr.svg
+        // Request Method: GET
+
+        cy.intercept({
+            method: 'GET',
+            hostname: 'res.cloudinary.com'
+        }, {
+            statusCode: 200,
+            fixture: 'images'
+        }).as('cloudinary')
+    });
+
+    it('Login com usuário e senha válidos', () => {
+        cy.login()
+
+        cy.visit('/')
+
+        cy.get('nav ul li')
+            .should('be.visible')
+            .and('have.length', 6)
+
+    })
+})
